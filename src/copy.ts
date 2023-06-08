bcdd9a871ee984594f74b6d5af35d7fd7ccde4a4
Bismark Arroyo <bis.arroyo@gmail.com>
2023-06-08 09:47:13 -0600

    Add copy to clipboard functionality

---
+++ src/copy.ts
@@ -0,0 +1,19 @@
+function copyToClipboard(elementId: string) {
+  const element = document.getElementById(elementId) as HTMLInputElement
+
+  if (element) {
+    const content = element.value
+
+    console.log(content)
+    navigator.clipboard
+      .writeText(content)
+      .then(() => {
+        alert('Contenido copiado al portapapeles: ' + content)
+      })
+      .catch((error) => {
+        console.error('Error al copiar al portapapeles:', error)
+      })
+  }
+}
+
+export default copyToClipboard
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import './style.css'
+import copyToClipboard from './copy.ts'
 
 // const app = document.querySelector('#app') as HTMLDivElement
 
 // const inputUppercase = document.querySelector(
 //   '#uppercaseText'
 // ) as HTMLInputElement
+
+const copyButton = document.getElementById('copyClipboard')
+
+copyButton?.addEventListener('click', () => {
+  copyToClipboard('password-generated')
+})

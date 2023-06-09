472447945a3b4dff04e5c4512a58c114c79eeafa
Bismark Arroyo <bis.arroyo@gmail.com>
2023-06-08 22:18:45 -0600

    add logic for password generator

--- src/generatePassword.ts
+++ src/generatePassword.ts
@@ -3,6 +3,57 @@ function randomCharacter(min: number, max: number): number {
   return randomNumber
 }
 
-const number: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const randomNumber = randomCharacter(0, number.length)
-console.log(randomNumber)
+// set all characters for password
+let number: string[] = []
+let specialCharacters: string[] = ['!', '@', '#', '$', '%', '&', '*', '-', '_']
+let lowarcaseLetter: string[] = []
+let uppercaseLetter: string[] = []
+
+// add number from loop
+for (var i = 0; i <= 9; i++) {
+  number.push(i.toString())
+}
+// add lowercase letters from ascii
+for (var i = 97; i <= 122; i++) {
+  lowarcaseLetter.push(String.fromCharCode(i))
+}
+// add uppercase letters from ascii
+for (var i = 65; i <= 90; i++) {
+  uppercaseLetter.push(String.fromCharCode(i))
+}
+
+function passwordGenerator({
+  length,
+  uppercase,
+  numbers,
+  specialCharacter
+}: {
+  length: number
+  uppercase: boolean
+  numbers: boolean
+  specialCharacter: boolean
+}) {
+  let password: string = ''
+  let characters: string[] = []
+
+  characters = characters.concat(lowarcaseLetter)
+
+  if (uppercase) {
+    characters = characters.concat(uppercaseLetter)
+  }
+  if (numbers) {
+    characters = characters.concat(number)
+  }
+  if (specialCharacter) {
+    characters = characters.concat(specialCharacters)
+  }
+
+  for (var i = 0; i < length; i++) {
+    const randomNumber = randomCharacter(0, characters.length)
+    password += characters[randomNumber]
+  }
+
+  return password
+}
+
+export default passwordGenerator
